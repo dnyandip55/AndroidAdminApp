72507a3e437814830f0b203b7e270ec049b7ce5a
Dnyandip Jadhav <jadhavvg77@gmail.com>
2024-02-17 17:13:34 +0530

    Admin App Final

--- app/src/main/java/com/example/adminapp/MainActivity.java
+++ app/src/main/java/com/example/adminapp/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.cardview.widget.CardView;
 import android.util.Log;
 
 import com.example.adminapp.faculty.UpdateFaculty;
+import com.example.adminapp.notice.DeleteNoticeActivity;
+import com.example.adminapp.notice.UploadNotice;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int ADD_NOTICE_ID = R.id.addNotice;
@@ -16,8 +18,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int  ADD_EBOOK=R.id.addEbook;
 
     private  static  final  int ADD_FACULTY=R.id.faculty;
+    private static  final int DELETE_NOTICE=R.id.deleteNotice;
 
-    CardView uploadNotice, addGalleryImage, addEbook, faculty;
+    CardView uploadNotice, addGalleryImage, addEbook, faculty,deleteNotice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         faculty = findViewById(R.id.faculty);
         faculty.setOnClickListener(this);
+
+        deleteNotice=findViewById(R.id.deleteNotice);
+        deleteNotice.setOnClickListener(this);
     }
 
     @Override
@@ -56,7 +62,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }else if (view.getId() == ADD_FACULTY) {
             // Handle click for addGalleryImage
             startActivity(new Intent(MainActivity.this, UpdateFaculty.class));
+        }else if (view.getId() == DELETE_NOTICE) {
+            // Handle click for addGalleryImage
+            startActivity(new Intent(MainActivity.this, DeleteNoticeActivity.class));
         }
-        // Add more else-if conditions for other views if needed
+
     }
 }
--- app/src/main/java/com/example/adminapp/notice/UploadNotice.java
+++ app/src/main/java/com/example/adminapp/notice/UploadNotice.java
@@ -1,4 +1,4 @@
-package com.example.adminapp;
+package com.example.adminapp.notice;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +17,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.adminapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
